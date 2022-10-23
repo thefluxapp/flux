bfdd56546605703533f9186e84b403cd661ed464
Andy Braga <bragovo@gmail.com>
2022-10-23 17:18:17 +0300

    Some tune

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { SafeAreaView, Text, View } from "react-native"
+
+export const App = () => {
+  // const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello flux!</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
